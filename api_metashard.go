ce857adcecdd832fbafd83a7a69f7e448bbafcf7
Echo <echo@Echos-MacBook-Pro.local>
2022-01-11 00:11:58 +0000

    add send method

--- api_metashard.go
+++ api_metashard.go
@@ -6,22 +6,17 @@ import (
 	"fmt"
 )
 
-func (self *Client) Upload(clientKey, clientSecret, code string) error {
+func (self *Client) Upload() error {
 	methodName := "Upload"
 
-	payload := map[string]string{
-		"clientKey":    clientKey,
-		"clientSecret": clientSecret,
-		"code":         code,
-	}
+	payload := map[string]string{}
 	b, err := json.Marshal(payload)
 	if err != nil {
 		return fmt.Errorf("%s: %s", methodName, err.Error())
 	}
-	println(string(b))
 	_, err = self.do(
 		"POST",
-		"v1/oauth/token",
+		"v1/upload",
 		bytes.NewBuffer(b),
 		self.POSTHeaders(),
 	)
---
+++ api_transaction.go
@@ -0,0 +1,43 @@
+package relysia
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+func (self *Client) Send(walletID, address, currencyType, notes string, amount float64) error {
+	methodName := "Send"
+
+	headers := Headers{
+		"type":     currencyType,
+		"walletID": walletID,
+		"notes":    notes,
+	}
+
+	b, _ := json.Marshal(
+		map[string]string{
+			"address": address,
+			"amount":  strconv.FormatFloat(amount, 'f', -1, 64),
+		},
+	)
+	println("SEND", string(b))
+
+	b, err := self.do(
+		"POST",
+		"v1/send",
+		bytes.NewBuffer(b),
+		self.POSTHeaders(headers),
+	)
+	if err != nil {
+		return fmt.Errorf("%s: %s", methodName, err.Error())
+	}
+
+	var response interface{} = new(interface{})
+	if err := json.Unmarshal(b, response); err != nil {
+		return fmt.Errorf("%s: %s", methodName, err.Error())
+	}
+
+	return nil
+}
