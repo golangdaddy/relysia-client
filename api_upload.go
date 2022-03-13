72c52f535c728f7779291f38f531c584f72da897
Alexander Breadman <alexanderbreadman@Alexanders-Mac-mini.local>
2022-03-13 00:41:50 +0000

    upload

---
+++ api_upload.go
@@ -0,0 +1,42 @@
+package relysia
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+func (self *Client) UploadReference(walletID, filename, url, notes string) error {
+	methodName := "Upload"
+
+	headers := Headers{
+		"walletID": walletID,
+	}
+
+	b, _ := json.Marshal(
+		map[string]string{
+			"type":     "media",
+			"fileUrl":  url,
+			"filename": filename,
+			"notes":    notes,
+		},
+	)
+	println("UPLOAD", string(b))
+
+	b, err := self.do(
+		"POST",
+		"v1/upload",
+		bytes.NewBuffer(b),
+		self.POSTHeaders(headers),
+	)
+	if err != nil {
+		return fmt.Errorf("%s: %s", methodName, err.Error())
+	}
+
+	var response interface{} = new(interface{})
+	if err := json.Unmarshal(b, response); err != nil {
+		return fmt.Errorf("%s: %s", methodName, err.Error())
+	}
+
+	return nil
+}
